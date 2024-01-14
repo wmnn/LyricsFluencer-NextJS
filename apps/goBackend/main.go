dbf79260761023c6a055e9966192116306425001
Peter Christian Würdemann <christian.wuerdemann@gmail.com>
2024-01-14 14:07:24 +0100

    added go echo framework

---
+++ apps/goBackend/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"net/http"
+	"github.com/labstack/echo/v4"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+func loadEnv() {
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		log.Fatal("Error loading .env file.")
+	}
+}
+
+func main() {
+	loadEnv()
+
+	e := echo.New()
+	e.HideBanner = false
+	e.HidePort = true
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+	e.Logger.Fatal(e.Start(":" + port))
+
+}
